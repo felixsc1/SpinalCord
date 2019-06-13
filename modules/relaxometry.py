129afb9eddf01de1329221dc9ea1c64732ad34b9
Felix <schlegel.felix@gmail.com>
2019-06-13 09:26:40 +0200

    cleaned up analysis, started some ML tests

--- modules/relaxometry.py
+++ modules/relaxometry.py
@@ -1,14 +1,31 @@
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as ss
+import warnings  # for sns joinplot, its complaining
 
-def getExcel(excelfile): 
-    info = pd.read_excel(excelfile, convert_float=True)
+from collections import defaultdict
+
+
+def getExcel(excelfile, header=0): 
+    info = pd.read_excel(excelfile, convert_float=True, header=header)
     try:
         info.columns = map(str.lower, info.columns)
     except:
-        print('column titles should not start with numbers')
+        pass
+#         print('column titles should not start with numbers')
     return(info)  
+
+
+def nested_dict(n, type):
+    # see: https://stackoverflow.com/questions/29348345/declaring-a-multi-dimensional-dictionary-in-python
+    if n == 1:
+        return defaultdict(type)
+    else:
+        return defaultdict(lambda: nested_dict(n-1, type))
+
 
 
 def cleanup_dimitri(df):
@@ -54,6 +71,46 @@ def cleanup_dimitri(df):
     return df
 
 
+
+
+def DTI_cleanup(DTI_raw):
+    
+    DTI_raw.iloc[:,1] = DTI_raw.iloc[:,1].astype(str) # assuming the scan infos are always in 2nd column
+
+    info_rows = DTI_raw.iloc[:,1].str.contains('/.+/.+/')   # looking for something in form of scan/Sclice location/number/matter type   40/C6/139/GM
+
+    import re
+
+    # constructing a clean dataframe:
+    DTI = pd.DataFrame()    
+    split_info = pd.DataFrame()    
+    for index, row in DTI_raw.iterrows():
+        if info_rows[index]:
+            date_id = re.search('_(20\d\d\d\d.*)_(\d\d\d)_', DTI_raw.iloc[index-1,1]) # this should be one row above the other infos, (regex: date is followed by _ and 3-digit animal ID)
+            DTI.loc[index,'id'] = int(date_id.group(2))
+            DTI.loc[index,'date'] = date_id.group(1)
+            
+            # continued: scan/Sclice location/number/matter type   40/C6/139/GM
+            split_info = DTI_raw.iloc[index,1].split('/') 
+            DTI.loc[index,'scan'] = split_info[0]
+            DTI.loc[index,'segment'] = split_info[1]
+            DTI.loc[index,'tissue'] = split_info[3]
+
+            # now the values (warning lot of positional numbers hardcoded here)
+            DTI.loc[index,'area'] = DTI_raw.iloc[index+1,2]
+            DTI.loc[index,'AD'] = DTI_raw.iloc[index+1,3]
+            DTI.loc[index,'FA'] = DTI_raw.iloc[index+2,3]
+            DTI.loc[index,'MD'] = DTI_raw.iloc[index+3,3]
+            DTI.loc[index,'RD'] = DTI_raw.iloc[index+4,3]
+            DTI.loc[index,'comments'] = DTI_raw.iloc[index,8]
+    return DTI
+
+
+
+
+
+
+
 def insert_BBB_data(df, bbbfile):
 	"""
 	just to declutter notebook.
@@ -88,13 +145,14 @@ def contrast_segment_selection(df, selection, columns):
 	return df_selection
 
 
+
 def outlier_removal(df_selection, df, testcols, SD_cutoff=2, removerow=False):
 	"""
 	With removerow=True, complete row is removed if any of testcols is outlier.
 	With removerow=False, all rows are kept, outliers just replaced by NaN (let plotting, stat functions deal with it hopefully...)
 	"""
-
-	print('Any NaNs? ', df_selection.isnull().values.any())
+    
+#     print('Any NaNs? ', df_selection.isnull().values.any())
 	    # df_selection.info()
 	    # print(df_selection.describe())
 	    # df_selection.hist();
@@ -110,7 +168,7 @@ def outlier_removal(df_selection, df, testcols, SD_cutoff=2, removerow=False):
 		    
 		    df_selection = df_selection[s]
 		    outlier_id = s[s == False]
-		    print(f"removed outliers for column: {c} \n {df.loc[outlier_id.index, ['comments','id']]}")
+#             print(f"removed outliers for column: {c} \n {df.loc[outlier_id.index, ['comments','id']]}")
 	else:
 		for c in testcols:
 		    s = ((df_selection[c] - df_selection[c].mean()) / df_selection[c].std()).abs() < SD_cutoff
@@ -118,10 +176,69 @@ def outlier_removal(df_selection, df, testcols, SD_cutoff=2, removerow=False):
 		    df_selection.loc[~s,c] = np.NaN
 		    #df_selection[c] = np.NaN
 		    outlier_id = s[s == False]
-		    print(f"removed outliers for column: {c} \n {df.loc[outlier_id.index, ['comments','id']]}")
+#             print(f"removed outliers for column: {c} \n {df.loc[outlier_id.index, ['comments','id']]}")
           
 	#df_selection.hist();
 	#plt.suptitle('Without outliers:', x=0.5, y=1.05, fontsize='large')
 	return df_selection
 
 
+## PLOTS ######
+                  
+                  
+def jointplot_corr_DTI(df, segment='T6', tissue='GM', x='AD', y='BBB_day1'):
+    """
+    called with interact
+    x and y are two column names within df.
+    """
+    warnings.filterwarnings("ignore")
+    
+    df_plot = df[(df['segment'] == segment) & (df['tissue'] == tissue)]
+    try:
+        g = sns.jointplot(x, y, data=df_plot, kind="reg", color="b", height=7,
+#                     xlim=(np.round(0.9*df_plot[x].min()), np.round(1.1*df_plot[x].max())),
+                       ylim=(np.round(0.8*df_plot[y].min()-1), np.round(1.2*df_plot[y].max())))
+#                         .set_axis_labels("x", "y")) #add ( at beginning (sns...
+        g.annotate(ss.pearsonr)
+    except TypeError:
+        print('column selection invalid')
+                  
+                  
+def jointplot_corr(dfall, contrast, segment, x, y):
+    """
+    called with interact
+    x and y are two columns within df.
+    """
+    warnings.filterwarnings("ignore")
+    df = dfall[contrast][segment]
+    try:
+        g = sns.jointplot(x, y, data=df, kind="reg", color="b", height=7,
+                       xlim=(np.round(0.95*df[x].min()-1), 1.05*df[x].max()+1),
+                       ylim=(np.round(0.95*df[y].min()-1), 1.05*df[y].max()+1))
+#                         .set_axis_labels("x", "y")) #add ( at beginning (sns...
+        g.annotate(ss.pearsonr)
+#     plt.show()
+
+    except TypeError:
+        print('column selection invalid')
+        
+        
+        
+def plot_boxplots_interactive(dfall, contrast, segment, columnI):
+    df = dfall[contrast][segment]
+    
+    df.dropna(subset=[columnI], inplace=True)   #<--crucial for ANOVA!
+    #why not dropping earlier? because now we look at only one column. For all others, the animal is still used.
+    
+    defpal = sns.color_palette("tab10")
+    newPal = dict(sham = defpal[2], mild = defpal[0], mod = defpal[1], sev = defpal[3])
+    
+    plt.figure(figsize=(7,7))
+    sns.boxplot(x="group", y=columnI, data=df, order=['sham', 'mild', 'mod', 'sev'], palette=newPal)
+    sns.despine(trim=False, left=True)
+
+    # ANOVA part:
+    groupedData = df.groupby('group')[columnI].apply(list)
+    groupedData.fillna(0, inplace=True)
+    F, p = ss.f_oneway(*groupedData)
+    print(f"ANOVA for {columnI}: F = {F}, p = {p}")
