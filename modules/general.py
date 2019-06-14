39951074a431f6eca18a322c38da98e471c4bd1a
Felix <schlegel.felix@gmail.com>
2019-06-14 16:13:41 +0200

    batch conversion, more ML tests

--- modules/general.py
+++ modules/general.py
@@ -1,3 +1,4 @@
+import os, glob, fnmatch
 import subprocess
 from numpy import genfromtxt
 import warnings
@@ -17,3 +18,24 @@ def extractROI(roifile, data, outfile):
     warnings.simplefilter("ignore", RuntimeWarning)  #ignore the warning for line below (string values cant be evaluated)
     roi_numbers = roidata[roidata>0]
     return roi_numbers
+
+
+def get_filepaths(mainpath, fileending='subject', subfolders=True):
+    """
+    Finds the files in ALL subfolders. (also used in other repos of mine)
+    """
+    filepaths = []
+    
+    if subfolders:
+        for folderName, _subfolders, filenames in os.walk(mainpath):
+            for file in filenames:
+                if fnmatch.fnmatch(file, fileending):
+                    filepaths.append(os.path.join(os.path.abspath(folderName),file))
+    
+    else:   
+        filepaths = glob.glob(os.path.join(os.path.abspath(mainpath),fileending))
+
+#     print('found: \n', filepaths)
+            
+  
+    return filepaths
